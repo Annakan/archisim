4567d51d7809f2b3e6b509c31b345fbc7aedcbe4
Annakan <duncanidaho@vectorball.com>
2015-09-09 19:43:29 +0200

    First draft of the functionnality and argument parser associated with it

--- lxdvm/spawn.py
+++ lxdvm/spawn.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
 
 import sys
 from jinja2 import Environment, FileSystemLoader
 from sh import lxc, sudo, ssh_keygen
 import time
 import os.path
+
+import argparse
+
+
+TEMPLATE_DIR = ""
+UID = GUID = 10000
+PRIVATE_NETWORK = "192.168.0.0"
+
+parser = argparse.ArgumentParser(description='Create LXD VM and register them dynamically')
+parser.add_argument("vm_names", help="name of  VMs to create (hostname)", type=basestring, nargs='+' )
+os_group = parser.add_argument_group("OS params", "parameters for OS type, versions and architecture")
+os_group.add_argument("--os", help="OS version", type=basestring, default='centos')
+os_group.add_argument("--release", help="OS release", type=basestring, default='7')
+os_group.add_argument("--arch", help="OS architecture", type=basestring, default='amd64')
+
+salt_group = parser.add_argument_group("salt params", "parameters for salt configuration of VMs")
+salt_group.add_argument("--salty", help="Make it a salt minion for the given master", type=basestring)
+salt_group.add_argument("--sal_id", help="salt id, if not present the machine hostname is used", type=basestring)
+salt_group.add_argument("--grain", help="register the given grain for that minion (require --salty)", type=basestring,
+                        action='append')
+
+parser.add_argument("--template", help="use the given file template to get options, "
+                                       "command line arguments will override template values", type=basestring,
+                    action='append')
 
 
 def list_vm():
@@ -16,8 +41,25 @@ def list_vm():
 
 vms = list_vm()
 
+
+def remove_grains(vm):
+    # check salt
+    # get true id
+    # remove grains
+    pass
+
+
+def register_in_salt_master():
+    pass
+
+
+def make_salt_master():
+    pass
+
+
 for vm in sys.argv[1:]:
     if vm in vms:
+        remove_grains(vm)
         lxc.delete(vm)
 
     if os.path.exists('home/vagrant/.ssh/known_hosts'):
